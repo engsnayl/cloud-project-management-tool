4bdfa456a1069a037566cbded0728a7859cb5ea0
engsnayl <engsnayl@gmail.com>
2025-09-01 07:34:41 +0000

    fix: Add null provider for Lambda module

--- terraform/modules/lambda/patch.tf
+++ terraform/modules/lambda/patch.tf
@@ -1,32 +1,46 @@
 # terraform/modules/lambda/patch.tf
 
-# Create empty zip files for Lambda functions if they don't exist
-resource "local_file" "create_api_handler_zip" {
-  filename = var.api_handler_zip_path
-  content  = "dummy content - created by Terraform"
-  
-  # Only create the file if it doesn't exist
+# Create valid minimal Lambda zip files
+resource "null_resource" "create_api_handler_zip" {
+  # Create a unique ID for this resource each time the zip path changes
+  triggers = {
+    zip_path = var.api_handler_zip_path
+  }
+
+  # Create a valid Lambda zip file
   provisioner "local-exec" {
-    command = "touch ${var.api_handler_zip_path}"
+    command = <<-EOT
+      mkdir -p $(dirname ${var.api_handler_zip_path})
+      cd $(dirname ${var.api_handler_zip_path})
+      echo 'def lambda_handler(event, context):\n    return {"statusCode": 200, "body": "Hello from Lambda!"}' > lambda_function.py
+      zip -r $(basename ${var.api_handler_zip_path}) lambda_function.py
+      rm lambda_function.py
+    EOT
     interpreter = ["bash", "-c"]
-    on_failure = continue
   }
 }
 
-resource "local_file" "create_document_processor_zip" {
-  filename = var.document_processor_zip_path
-  content  = "dummy content - created by Terraform"
-  
-  # Only create the file if it doesn't exist
+resource "null_resource" "create_document_processor_zip" {
+  # Create a unique ID for this resource each time the zip path changes
+  triggers = {
+    zip_path = var.document_processor_zip_path
+  }
+
+  # Create a valid Lambda zip file
   provisioner "local-exec" {
-    command = "touch ${var.document_processor_zip_path}"
+    command = <<-EOT
+      mkdir -p $(dirname ${var.document_processor_zip_path})
+      cd $(dirname ${var.document_processor_zip_path})
+      echo 'def lambda_handler(event, context):\n    return {"statusCode": 200, "body": "Document processed!"}' > lambda_function.py
+      zip -r $(basename ${var.document_processor_zip_path}) lambda_function.py
+      rm lambda_function.py
+    EOT
     interpreter = ["bash", "-c"]
-    on_failure = continue
   }
 }
 
-# Update Lambda function variables to depend on the local files
+# Update Lambda function variables to depend on the null resources
 locals {
-  api_handler_zip_path = local_file.create_api_handler_zip.filename
-  document_processor_zip_path = local_file.create_document_processor_zip.filename
+  api_handler_zip_path = var.api_handler_zip_path
+  document_processor_zip_path = var.document_processor_zip_path
 }
