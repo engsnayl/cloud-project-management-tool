4c22e2023b501185d772d74c36eba4d710e771d0
engsnayl <engsnayl@gmail.com>
2025-09-02 07:21:38 +0000

    fix: Add Lambda source files for CI/CD testing

---
+++ src/lambdas/api-handler/lambda_function.py
@@ -0,0 +1,1 @@
+def lambda_handler(event, context):\n    return {"statusCode": 200, "body": "Hello from Lambda!"}
---
+++ src/lambdas/document-processor/lambda_function.py
@@ -0,0 +1,1 @@
+def lambda_handler(event, context):\n    return {"statusCode": 200, "body": "Document processed!"}
